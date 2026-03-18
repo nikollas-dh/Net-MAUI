a082ad2f9d5a14cc1df6d8df511a431ccc72a8ff
unknown <dev@DSN1191348097.edu119.local>
2026-03-18 16:25:59 -0300

    Configuração Inicial Consumo de API

---
+++ Telas/Platforms/Services/ApiService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telas.Platforms.Services
+{
+   public static class ApiService<T> where T: class
+    {
+        private static HttpClient client;
+
+        public static HttpClient Client
+        {
+            get
+            {
+                if (client == null) 
+                { 
+                    client = new HttpClient();
+                    client.BaseAddress = new url("http://127.0.0.1:5500/api/")
+                }
+                return client;
+            }
+            public async static Task<List<T>> GetList()
+        }
+
+    }
+}
